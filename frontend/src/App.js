edb4cab2a4b41eadcda8d01c08f3dc1e26afe137
Luca D'Angelo <info@lucadangelo.it>
2023-10-11 07:10:17 +0100

    App.Js Route, Default Redirection, Switch wrapper, NewSurfPlace Scaffolding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'; //LD page routing - https://www.w3schools.com/react/react_router.asp
+//LD page routing - https://www.w3schools.com/react/react_router.asp
+//LD 
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'; 
 
 import Users from './user/pages/Users';
+import NewSurfPlace from './surfPlaces/pages/NewSurfPlace';
 
 function App() {
   return <Router>
-              <Route path="/"> 
-                <Users />
-              </Route>
+            <Switch>{/*instructions evaluated in sequence, need to use "switch". https://www.freecodecamp.org/news/react-router-cheatsheet/ Inside this block if meet routing condition, will stop evaluating next step*/}
+          
+                      <Route path="/" exact={true}> {/* LD with "exact", if the path is exactly "http://localhost:3000/" it render the "User.js" page*/}
+                           <Users />
+                      </Route>
+                      <Route path="/surfplaces/new" exact={true}> 
+                           <NewSurfPlace /> 
+                      </Route>
+                      <Redirect to="/"/> {/*If path is anything else then redirect. Source https://v5.reactrouter.com/web/api/Redirect*/}
+            </Switch>             
         </Router>
+        
+
+
+
+
 }
 
 export default App;
---
+++ frontend/src/surfPlaces/pages/NewSurfPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewSurfPlace = () => {
+    return <h2>areat will be used to add a new visited surf place</h2>
+};
+
+export default NewSurfPlace;
