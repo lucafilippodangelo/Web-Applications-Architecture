7d0ea998adb6ae724714fe817f4e6a2fb92103b8
Luca D'Angelo <info@lucadangelo.it>
2023-10-13 05:25:13 +0100

    Implemented Navigation side drawer (used react-transition-group)

---
+++ frontend/src/shared/components/UI/Backdrop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './Backdrop.css';
+
+const Backdrop = props => {
+  return ReactDOM.createPortal(
+    //LD the logic to close the drawer is on the parent component "MainNavigation"
+    // so below we emit a function we expect to be on the props we receiving.
+    // in summary we expect the caller to have an "onClick" button.
+    <div className="backdrop" onClick={props.onClick}></div>, 
+
+    //LD below I utilise the portal technique. Will then render this component in index.html
+    document.getElementById('backdrop-hook')
+  );
+};
+
+export default Backdrop;
--- frontend/src/shared/components/navigation/Navigation.js
+++ frontend/src/shared/components/navigation/Navigation.js
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import Header from './Header';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UI/Backdrop';
+
 import './Navigation.css';
 
-const MainNavigation = props => {
+const Navigation = props => {
+
+    //going to use STATE for the drawer
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false); //by default "drawerIsOpen=false"
+
+
+  //creating a function to OPEN and CLOSE the drawer.
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  }
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  }
+
+
+
+
   return (
-    <Header>
-        {/* 
-        LD the content inside div "Header" will go to fill what is in 
-        "props.children" of the component "Header.js" (so the child).
-        https://medium.com/@martin.crabtree/react-js-using-children-props-c83d5b259756#:~:text=The%20%7B%20props.,be%20rendered%20by%20the%20child.
-        */}
-        <button className="navigation__menu-btn">
-          <span />
-          <span />
-          <span />
-        </button>
-        <h1 className="navigation__title">
-          <Link to="/">Surf Places</Link>
-        </h1>
-        <nav>
-            <NavLinks />
-        </nav>
-    </Header>
+    <React.Fragment> {/*LD Useful to wrap two header divs. JSX allow one single entrance point */}
+
+        {/* render "Backdrop" if true */}            
+        {drawerIsOpen && <Backdrop onClick={closeDrawer}/>} 
+
+        {/* the transition logic in "SideDrawer" component depends from the value received in props "show"*/}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+            <nav className="navigation__drawer-nav">
+                <NavLinks />
+            </nav>
+        </SideDrawer> 
+
+        <Header>
+            {/* 
+            LD the content inside div "Header" will go to fill what is in 
+            "props.children" of the component "Header.js" (so the child).
+            https://medium.com/@martin.crabtree/react-js-using-children-props-c83d5b259756#:~:text=The%20%7B%20props.,be%20rendered%20by%20the%20child.
+            */}
+            <button className="navigation__menu-btn" onClick={openDrawer}> 
+            <span />
+            <span />
+            <span />
+            </button>
+            <h1 className="navigation__title">
+            <Link to="/">Surf Places</Link>
+            </h1>
+            <nav className="navigation__header-nav">
+                <NavLinks />
+            </nav>
+        </Header>
+    </React.Fragment>  
   );
 };
 
-export default MainNavigation;
+export default Navigation;
