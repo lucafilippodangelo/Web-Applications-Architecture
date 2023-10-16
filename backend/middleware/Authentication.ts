4d086f62bdceb3031532b0fcc985815aa886f57f
Dylan Caulfield <dpcdylan@gmail.com>
2023-10-16 15:10:54 +0100

    Add authentication middleware

---
+++ backend/middleware/Authentication.ts
@@ -0,0 +1,26 @@
+import {Request, Response, NextFunction} from "express";
+import {decode, verify} from "jsonwebtoken";
+import StatusCodes from "http-status-codes";
+import {IUser} from "../core/IUser";
+
+export default function authenticate(req: Request, res: Response, next: NextFunction) {
+
+    const token = req.header("Authorization");
+    if(!token) return unauthorized(res);
+
+    try {
+        req.user = verify(token, process.env.JWT_SECRET!) as IUser;
+        next();
+    } catch {
+        return unauthorized(res);
+    }
+
+}
+
+function unauthorized(res: Response){
+
+    res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Invalid credentials provided"
+    });
+
+}
