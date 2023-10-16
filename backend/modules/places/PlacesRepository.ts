a3d825c175f4e8cb3a5a959b914360673145ebe2
Dylan Caulfield <dpcdylan@gmail.com>
2023-10-16 15:10:09 +0100

    Makes PlacesRepository a class with methods to prevent duplication of function names between service and repository layer

--- backend/modules/places/PlacesRepository.ts
+++ backend/modules/places/PlacesRepository.ts
@@ -1,22 +1,35 @@
 import {IPlace, PlaceModel} from "../../models/Place";
 import ICoordinates from "../../core/ICoordinates";
 import {Document, ObjectId} from "mongoose";
+import exp from "constants";
 
-export async function FindAllPlaces(): Promise<IPlace[]> {
+export interface IPlacesRepository {
+    findAllPlaces(): Promise<IPlace[]>
+    findAllPlacesSortedByDistanceFromCoordinates(coordinates: ICoordinates): IPlace[]
+    createPlace(place: IPlace): Promise<void>
+}
 
-    return PlaceModel.find({});
+class PlacesRepositoryInternal implements IPlacesRepository {
+
+    findAllPlaces(): Promise<IPlace[]> {
+
+        return PlaceModel.find({});
+
+    }
+
+    findAllPlacesSortedByDistanceFromCoordinates(coordinates: ICoordinates): IPlace[] {
+
+        throw Error("Not implemented");
+
+    }
+
+     async createPlace(place: IPlace): Promise<void> {
+
+        const placeModel = new PlaceModel(place);
+        await placeModel.save();
+
+    }
 
 }
 
-export function FindAllPlacesSortedByDistanceFromCoordinates(coordinates: ICoordinates): IPlace[] {
-
-    throw Error("Not implemented");
-
-}
-
-export async function CreatePlace(place: IPlace): Promise<void> {
-
-    const placeModel = new PlaceModel(place);
-    await placeModel.save();
-
-}
+export const PlacesRepository = new PlacesRepositoryInternal() as IPlacesRepository
--- backend/modules/places/services/FindAllPlacesService.ts
+++ backend/modules/places/services/FindAllPlacesService.ts
@@ -1,5 +1,5 @@
-import {FindAllPlaces as FindAllPlacesQuery} from "../PlacesRepository";
 import {IPlace} from "../../../models/Place";
+import {PlacesRepository} from "../PlacesRepository";
 
 export interface IFindAllPlacesCommand {
     latitude?: number,
@@ -8,14 +8,12 @@ export interface IFindAllPlacesCommand {
 
 export default function FindAllPlaces(command: IFindAllPlacesCommand): Promise<IPlace[]> {
 
-    throw new Error("hi");
-
     const useLocationBias = Boolean((command.latitude) && (command.longitude));
 
     if (useLocationBias) {
 
     }
 
-    return FindAllPlacesQuery();
+    return PlacesRepository.findAllPlaces();
 
 }
