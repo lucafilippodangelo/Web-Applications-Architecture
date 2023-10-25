958341716bbc305973ec6a3a0ca2ec2b449787de
Luca D'Angelo <info@lucadangelo.it>
2023-10-25 09:04:52 +0100

    Updated Surf User page to use http hook

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -3,53 +3,34 @@ import React, { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
 import ErrorModal from '../../shared/components/UI/ErrorM';
 import LoadingSpinner from '../../shared/components/UI/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 //LD NOTE -> send a request any time the page load 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+
   const [loadedUsers, setLoadedUsers] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   //LD useEffect
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const fetchUsers  = async () => {
       try {
-        const response = await fetch('http://localhost:3001/api/users', {
-          mode:'cors',
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json' }
-        });
-        //LD do not need to add "HEADERS" because not sending to the server anything in input so no body to attach.
-        const responseData = await response.json();
-
-        console.log("-- LD LIST OF USERS below ");
-        console.log(responseData);
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
+        const responseData = await sendRequest(
+          'http://localhost:3001/api/users'
+        );
 
         setLoadedUsers(responseData);
-        
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
+        console.log(responseData);
+      } catch (err) { }
     };
-    sendRequest();
-  }, []); 
-  // LD if dependencies "[]" empty, the code inside the useEffect "{}"will run only once.
-  // NOTE doing the above I avoid to run fetch any time something change in user page.
+    fetchUsers ();
+  }, [sendRequest]); // LD "sendRequest" is now coming outside the "useEffect" that's why this is a dependency now
 
-  const errorHandler = () => {
-    setError(null);
-  };
+  
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center"> 
           <LoadingSpinner />
